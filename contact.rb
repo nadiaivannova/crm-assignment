9d2490e806f75e262f1553fc9680d9c190826dca
nadiaivannova <nadiaivannova@gmail.com>
2017-07-08 18:55:10 -0400

    had to redo repository as for got to fork

--- contact.rb
+++ contact.rb
@@ -1,58 +1,71 @@
-class Contact
+def initialize(first_name, last_name, email, notes = 'N/A')
+  @first_name = first_name
+  @last_name  = last_name
+  @email      = email
+  @notes      = notes
+  @id         = @@next_id
 
-  # This method should initialize the contact's attributes
-  def initialize
+  @@next_id += 1
+end
 
+def self.create(first_name, last_name, email, notes = 'N/A')
+  new_contact = Contact.new(first_name, last_name, email, notes)
+  @@contacts << new_contact
+  return new_contact
+end
+
+def self.all
+  @@contacts
+end
+
+def self.find(id)
+@@contacts.each do |contact|
+  if contact.id == id
+  return contact.full_name
   end
+end
+end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
+def update(field, new_value)
+case field
+when "first_name"
+  self.first_name = new_value
+when "last_name"
+  self.last_name = new_value
+when "email"
+  self.email = new_value
+when "notes"
+  self.notes = new_value
+else
+  puts " Error, please try again"
+end
+end
 
+def self.find_by(attribute, value)
+@@contacts.each do |contact|
+  if attribute == "first_name" && value ==        contact.first_name
+    return contact.full_name
+  elsif attribute == "last_name" && value == contact.last_name
+    return contact.full_name
+  elsif attribute == "email" && value == contact.email
+  elsif attribute == "notes" && value == contact.notes
+    return contact.full_name
+  else
+    puts "Error, please try again."
   end
+end
+end
 
-  # This method should return all of the existing contacts
-  def self.all
+def self.delete_all
+@@contacts.clear
+end
 
-  end
+def full_name
+"#{ @first_name } #{ @last_name }"
+end
 
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find
+def self.delete(contact)
+@@contacts.delete(contact)
+end
 
-  end
-
-  # This method should allow you to specify 
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update
-
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
-  end
-
-  # This method should delete all of the contacts
-  def self.delete_all
-
-  end
-
-  def full_name
-
-  end
-
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-
-  end
-
-  # Feel free to add other methods here, if you need them.
-  
 end
